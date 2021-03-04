e34a65af45f6a62bca98a18ea6a605306a6926b8
Patrik Slovic <patrikslovic@gmail.com>
2021-03-04 13:51:03 +0100

    Update code.js

--- JavaScript/Moving zeros to the end 5 kyu/code.js
+++ JavaScript/Moving zeros to the end 5 kyu/code.js
@@ -1,16 +1,5 @@
 let moveZeros = function (arr) {
-    let result = [];
-    let counter = 0;
-    for (const elem of arr) {
-        if (elem === 0) {
-            counter++;
-        } else {
-            result.push(elem);
-        }
-    }
-
-    for (let i = 0; i < counter; i++) {
-        result.push(0);
-    }
-    return result;
+    const withoutZerosArr = arr.filter(num => num !== 0);
+    const zerosArr = arr.filter(num => num === 0);
+    return withoutZerosArr.concat(zerosArr);
 }
